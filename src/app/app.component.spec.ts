dd68f2f3350590b6657a90b5c5dd4d61642ddfb0
vitaliymelamud <melamudv@gmail.com>
2021-11-16 21:51:16 +0200

    read file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nice'`, () => {
+  it(`should have as title 'bwr'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nice');
+    expect(app.title).toEqual('bwr');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('nice app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('bwr app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.less']
 })
   export class AppComponent implements OnInit{
-  title = 'nice';
+  title = 'BWR';
 
   constructor() {
   }
